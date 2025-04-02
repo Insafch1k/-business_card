483b2a1230cf245413769d8e143c159445bfdf40
Alexandra <ooorca@MacBook-Pro-Alexandra.local>
2025-04-02 21:41:34 +0300

    Добавлен проект vizitka

---
+++ vizitka/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+
+const routes: Routes = [
+  { 
+    path: '', 
+    component: MainComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'enabled'
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
