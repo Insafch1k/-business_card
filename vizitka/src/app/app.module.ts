0e641ad001052a8167be47dfb9603b9e5b44c50c
YOBLANA <salahovtagir@mail.ru>
2025-04-07 21:10:56 +0300

    добавил все оставшиеся компоненты в архитектуру

--- vizitka/src/app/app.module.ts
+++ vizitka/src/app/app.module.ts
@@ -10,13 +10,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { MainComponent } from './components/main/main.component';
+import { TeamComponent } from './components/team/team.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { QuestionsComponent } from './components/questions/questions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    TeamComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    ContactsComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
