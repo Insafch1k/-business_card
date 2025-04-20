0f2765742ab8eb2f902aad97eddb7e31e4e1e9fc
Alexandra <ooorca@MacBook-Pro-Alexandra.local>
2025-04-20 17:46:21 +0300

    переход по компонентам через футер

--- vizitka/src/app/app-routing.module.ts
+++ vizitka/src/app/app-routing.module.ts
@@ -3,17 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: MainComponent,
-  }
+  { path: '', component: MainComponent,},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    anchorScrolling: 'enabled',
-    scrollPositionRestoration: 'enabled'
-  })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
